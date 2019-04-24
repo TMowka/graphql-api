aeac95a09e483085267ee4c460e04394254bbdf3
TMowka <deftassassin@gmail.com>
2019-04-24 21:00:47 +0300

    feat: events mocked query, mutation implementation

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res, next) => {
-  res.send('Hello World!');
-});
+app.use(
+  '/graphql',
+  graphqlHttp({
+    schema: buildSchema(`
+      type RootQuery {
+        events: [String!]!
+      }
+
+      type RootMutation {
+        createEvent(name: String): String
+      }
+
+      schema {
+        query: RootQuery
+        mutation: RootMutation
+      }
+    `),
+    rootValue: {
+      events: () => {
+        return ['Romantic Cooking', 'Sailing', 'All-Night Coding'];
+      },
+      createEvent: ({ name }) => {
+        return name;
+      },
+    },
+    graphiql: true,
+  }),
+);
 
 app.listen(5000);
