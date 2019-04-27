ebd0824b834c95581576dc33e27c646d2f6a1999
TMowka <deftassassin@gmail.com>
2019-04-27 20:09:53 +0300

    [frontend] feat: MainNavigation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import {
   BrowserRouter, Route, Redirect, Switch,
 } from 'react-router-dom';
+import './App.css';
 
+import MainNavigation from './components/Navigation/MainNavigation';
 import AuthPage from './pages/AuthPage';
 import EventsPage from './pages/EventsPage';
 import BookingsPage from './pages/BookingsPage';
@@ -10,12 +12,15 @@ import BookingsPage from './pages/BookingsPage';
 function App() {
   return (
     <BrowserRouter>
-      <Switch>
-        <Redirect from="/" to="/auth" exact />
-        <Route path="/auth" component={AuthPage} />
-        <Route path="/events" component={EventsPage} />
-        <Route path="/bookings" component={BookingsPage} />
-      </Switch>
+      <MainNavigation />
+      <main className="main-content">
+        <Switch>
+          <Redirect from="/" to="/auth" exact />
+          <Route path="/auth" component={AuthPage} />
+          <Route path="/events" component={EventsPage} />
+          <Route path="/bookings" component={BookingsPage} />
+        </Switch>
+      </main>
     </BrowserRouter>
   );
 }
